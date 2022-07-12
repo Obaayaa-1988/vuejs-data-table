0fd54fce51b25898ba35ebdf6e10c914ffd9168a
Abigail <ashbella333@gmail.com>
2022-07-12 16:22:31 +0000

    creted html, style, js files

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+        el:"#create",
+    
+        data: {
+          datas: [
+             {"id":3,"name":"Isaac Yeboah", "phone":"+233 54 241 3633","email":"isaac@walulel.com","admin_first_time":false,"avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/KOGitClVgN52hzmPnewqDVsWh.jpg","organization":null,"board_access_admin":false,"country":"GH","created_at":"2020-06-09T12:42:37.000000Z"},
+    
+             {"id":1,"name":"Beth Taiwo", "phone":"+233 20 341 3633","email":"elizabeth@walulel.com","admin_first_time":false,"avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/iHkEqfZhyOjqAi3DTaBGgcDel.jpg","organization":null,"board_access_admin":false,"country":"UK","created_at":"2022-07-05T16:58:09.000000Z"}
+    
+          ]
+        }
+    
+    });
